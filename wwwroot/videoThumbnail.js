1cea9eb7130e5b6845dcb13446de2471600ed1e1
pleshw <pleshw@outlook.com>
2023-09-04 05:46:02 -0300

    ajustando logica para mostrar thumb e dados do vídeo do youtube

---
+++ wwwroot/videoThumbnail.js
@@ -0,0 +1,26 @@
+﻿export function loadPopover(elementId, videoTitle, channelTitle, duration) {
+    const popover = new bootstrap.Popover(document.getElementById(elementId),
+        {
+            title: videoTitle,
+            trigger: 'hover focus',
+            sanitize: true,
+            placement: 'right',
+            customClass: 'video-thumbnail-popover',
+            html: true,
+            content: `<ul>
+                <li>Canal: ${channelTitle}</li>
+                <li>Duração: ${duration}</li>
+            </ul>`,
+        });
+}
+
+export function disposePopover(elementId) {
+    const elementPopover = document.getElementById(elementId);
+
+    if (elementPopover) {
+        const popoverInstance = bootstrap.Popover.getInstance(elementPopover);
+        if (popoverInstance) {
+            popoverInstance.dispose();
+        }
+    }
+}
