fcab93993d31d702ed078fcbf97d3ffbe8f4effd
pleshw <pleshw@outlook.com>
2023-09-08 01:06:47 -0300

    ajustes para o futuro!

--- Program.cs
+++ Program.cs
@@ -8,28 +8,20 @@ using static Clube.Shared.YoutubeDownloader;
 var builder = WebApplication.CreateBuilder( args );
 
 // Add services to the container.
+builder.Services.AddOptions();
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddSession();
+builder.Services.AddHttpClient();
+builder.Services.AddAuthorizationCore();
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
 builder.Services.AddTransient<IObserver<double> , YoutubeDownloader>();
-builder.Services.AddTransient<IProgress<double>, ProgressTracker>();
+builder.Services.AddTransient<IProgress<double> , ProgressTracker>();
 builder.Services.AddSingleton<YoutubeDownloaderService>();
 
-//builder.Services.AddAuthentication()
-//    .AddGoogle( googleOptions => {
-//        googleOptions.ClientId = builder.Configuration["Authentication:External:Google:ClientId"]!;
-//        googleOptions.ClientSecret = builder.Configuration["Authentication:External:Google:ClientSecret"]!;
-//    } )
-//    .AddTwitter( twitterOptions => {
-//        twitterOptions.ConsumerKey = builder.Configuration["Authentication:External:Twitter:ApiKey"]!;
-//        twitterOptions.ConsumerSecret = builder.Configuration["Authentication:External:Twitter:ApiSecret"]!;
-//    } )
-//    .AddFacebook( facebookOptions =>
-//    {
-//        facebookOptions.AppId = builder.Configuration["Authentication:External:Facebook:AppId"]!;
-//        facebookOptions.AppSecret = builder.Configuration["Authentication:External:Facebook:AppSecret"]!;
-//        facebookOptions.AccessDeniedPath = "/AccessDeniedPathInfo";
-//    } );
+
+builder.Configuration.AddUserSecrets<Program>();
 
 var app = builder.Build();
 
@@ -37,16 +29,18 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler( "/Error" );
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
-
 app.UseStaticFiles();
 
+app.UseSession();
 app.UseRouting();
+app.UseAuthentication();
+app.UseAuthorization();
 
+app.MapControllers();
 app.MapBlazorHub();
 app.MapFallbackToPage( "/_Host" );
 
